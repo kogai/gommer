80a2954f0e0e60490c78555bf849ee857806871e
Shinichi Kogai <kogai0121@gmail.com>
2015-10-25 22:48:44 +0900

    pubsub的なもので接続
    channel経由でやり取りした方がgoっぽいかも

--- gommer.go
+++ gommer.go
@@ -1,45 +1,30 @@
 package gommer
 
 import (
+	// "fmt"
 	// "github.com/mattn/go-pubsub"
 	"golang.org/x/mobile/app"
 	"golang.org/x/mobile/event/touch"
 )
 
-/**
-var gom gommer.Gommer = &{
-	//特定の領域のtouch.Eventを送ってくる
-	target: target,
-
-	// 必要なら
-	options: options,
-}
-
-var gom gommer.Gommer = gommer.New()
-
-gom.On("hold", func(event touch.Event){
-	// targetのイベントを判定
-})
-
-**/
-
 func New(a app.App) Manager {
 	m := Manager{
 		target: a,
 	}
-	go m.listen()
+	// go m.listen()
 	return m
 }
 
 type Manager struct {
 	target   app.App
-	handlers map[string]CallbackFns
+	handlers map[string]callbackFns
 	// c chan touch.Event
 }
 
-type CallbackFn func(event touch.Event)
-type CallbackFns []CallbackFn
+type callbackFn func(event touch.Event)
+type callbackFns []callbackFn
 
+/*
 func (m Manager) listen() {
 	for e := range m.target.Events() {
 		switch e := app.Filter(e).(type) {
@@ -48,14 +33,27 @@ func (m Manager) listen() {
 		}
 	}
 }
+*/
 
-func (m Manager) On(eventType string, callback CallbackFn) {
-	m.handlers[eventType] = append(m.handlers[eventType], CallbackFn)
+func (m Manager) On(eventType string, callback callbackFn) {
+	var callbacks callbackFns
+
+	if m.handlers[eventType] != nil {
+		callbacks = m.handlers[eventType]
+	}
+
+	callbacks = append(callbacks, callback)
+	m.handlers = map[string]callbackFns{
+		eventType: callbacks,
+	}
+	// m.emit(eventType)
 }
 
+/*
 func (m Manager) Off(eventType string) {
-	delete(m.handlers[eventType])
+	delete(m.handlers, eventType)
 }
+*/
 
 func (m Manager) emit(eventType string, event touch.Event) {
 	handlers := m.handlers[eventType]
